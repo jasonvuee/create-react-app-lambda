8bbd259c7b3d6cbf5410db395baf85e66b89dddf
jasonvuee <100932274+jasonvuee@users.noreply.github.com>
2022-03-06 18:07:22 +0900

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import React from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
@@ -6,7 +6,7 @@ import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
-    <Fragment>
+    <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton />
@@ -14,7 +14,7 @@ const Header = (props) => {
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
-    </Fragment>
+    </React.Fragment>
   );
 };
 
